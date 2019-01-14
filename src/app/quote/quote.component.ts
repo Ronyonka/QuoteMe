2cef3e6a2ffa8e85002834c90bb4de79936a57bc
Ronyonka <rononyonka@gmail.com>
2019-01-14 12:51:41 +0300

    resolved errors

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,11 @@ import{QuoteMe} from '../quote-me';
 })
 export class QuoteComponent implements OnInit {
    quotes = [
-    new QuoteMe("Any dog under 50 pounds is a cat and cats are pointless.", "Ron Swanson", "Ron Onyonka", new Date()),
-    new QuoteMe("Presents are the best way to show someone how much you care. It's like this tangible thing that you can point to and say, ‘Hey, man, I love you this many dollars worth.’","Michael Scott", "Ron Onyonka", new Date()),
-    new QuoteMe("A lie is just a great story that someone ruined with the truth.", "Barney Stinsen", "Ron Onyonka", new Date()),
-    new QuoteMe("Anyone can be a lawyer. You can even represent yourself. You can't do surgery on yourself. It's illegal. You'd get arrested, and then you'd get a free lawyer.", "Abed Nadir", "Ron Onyonka", new Date()),
-    new QuoteMe("It's not easy being drunk all the time. If it were easy, everyone would do it.", "Tyrion Lannister", "Ron Onyonka", new Date()),
+    new QuoteMe("Any dog under 50 pounds is a cat and cats are pointless.", "Ron Swanson", "Ron Onyonka", new Date(2018, 3, 14)),
+    new QuoteMe("Presents are the best way to show someone how much you care. It's like this tangible thing that you can point to and say, ‘Hey, man, I love you this many dollars worth.’","Michael Scott", "Ron Onyonka", new Date(2018, 3, 14)),
+    new QuoteMe("A lie is just a great story that someone ruined with the truth.", "Barney Stinsen", "Ron Onyonka", new Date(2018, 3, 14)),
+    new QuoteMe("Anyone can be a lawyer. You can even represent yourself. You can't do surgery on yourself. It's illegal. You'd get arrested, and then you'd get a free lawyer.", "Abed Nadir", "Ron Onyonka", new Date(2018, 3, 14)),
+    new QuoteMe("It's not easy being drunk all the time. If it were easy, everyone would do it.", "Tyrion Lannister", "Ron Onyonka", new Date(2018, 3, 14)),
 
   ]
   addNewQuoteMe(quote){
